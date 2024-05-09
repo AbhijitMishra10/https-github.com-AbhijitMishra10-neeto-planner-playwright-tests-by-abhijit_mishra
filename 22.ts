9186688ce020f4da56f3cd790b2a3c19ac35b4c7
AbhijitMishra10 <abhijitmishra2000@gmail.com>
2024-05-09 22:51:29 +0530

    Add files via upload

---
+++ 22.ts
@@ -0,0 +1,29 @@
+const { chromium } = require('playwright');
+
+(async () => {
+const browser = wait chromium.launch();
+const context = wait browser.newContext() ;
+const page = wait context.newPage();
+
+// enter your neetoPlanner subdomain
+wait page.goto('https://your-neetoPlanner-subdomain.com ' ); < br>
+// Enter your email
+wait page.fill('input[name="email"]', 'your-email@example.com');
+< br> // Enter a random OTP number
+const otp =generateRandomOTP(); // Use the function to generate a random OTP
+ait page.fill('input[name="otp"]', otp
+< br > / / Confirm login ID
+wait Promise.all([
+page.waitForNavigation(), // Wait for navigation to complete
+page.click('button[type ="submit"]' ) // Click submit button
+]);
+
+// Alternatively,
+> // You can add confirmation or additional tasks after closing the browser
+wait browser.close() ;
+}( );
+
+functiongenerateRandomOTP() {
+// Use logic to generate a random number OTP
+return Math.floor(100000 + Math. random() * 900000).toString();
+}
